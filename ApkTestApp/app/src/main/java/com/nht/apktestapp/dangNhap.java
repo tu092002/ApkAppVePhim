615ccc2d125a5dc177306a4d12b10f492825be20
phamtrungkienit02 <2051052067kien@ou.edu.vn>
2023-08-11 11:30:14 +0700

    kiểm tra không nhập username, password; nhập sai username, password

--- ApkTestApp/app/src/main/java/com/nht/apktestapp/dangNhap.java
+++ ApkTestApp/app/src/main/java/com/nht/apktestapp/dangNhap.java
@@ -16,9 +16,6 @@ public class dangNhap extends AppCompatActivity {
     EditText edtUsernameLogin, edtPasswordLogin;
     Button btnDangKyPage;
     Button btnDangNhap;
-    String taiKhoanHienTai, matKhauHienTai, hoTenHienTai;
-    int maUserHienTai;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,36 +29,22 @@ public class dangNhap extends AppCompatActivity {
         btnDangNhap = (Button) findViewById(R.id.btnDangNhap);
         btnDangKyPage = (Button) findViewById(R.id.btnDangKyPage);
 
-
-//        Intent i = getIntent(); // sài chung cho 2 cách
-////        String value1 = i.getStringExtra("Key1");
-////        int value2 = i.getIntExtra("Key2", 0);
-////
-////
-////        tv1.setText(value1);
-////        tv2.setText(""+value2);
-//
-//
-//        // cách 2  bằng Bundle
-//        Bundle b = i.getExtras();
-//        String value1 = b.getString("USERNAME");
-//        String value2 = b.getString("PASSWORD");
-//        edtUsernameLogin.setText(value1);
-//        edtPasswordLogin.setText(value2);
-
-        // btn dang nhap
-
         btnDangNhap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                String username = edtUsernameLogin.getText().toString();
-                String password = edtPasswordLogin.getText().toString();
                 try{
-                        User userLogin =  MainActivity.database.GetUserByUsername(username,password);
-                    Toast.makeText(dangNhap.this, "Chào mừng " + userLogin.getHoTen() + " quay trở lại!!!" , Toast.LENGTH_SHORT).show();
-                    startActivity(new Intent(dangNhap.this, MainActivity.class));
+                    String username = edtUsernameLogin.getText().toString();
+                    String password = edtPasswordLogin.getText().toString();
+                    if(username.equals("")||password.equals(""))
+                        Toast.makeText(dangNhap.this, "Chưa nhập username hoặc password", Toast.LENGTH_SHORT).show();
+                    else {
+                        if (MainActivity.database.checkLogin(username, password) == true) {
+                            User userLogin = MainActivity.database.GetUserByUsername(username, password);
+                            Toast.makeText(dangNhap.this, "Chào mừng " + userLogin.getHoTen() + " quay trở lại!!!", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(dangNhap.this, MainActivity.class));
 
+                        } else Toast.makeText(dangNhap.this, "Username hoặc Password sai!!!", Toast.LENGTH_SHORT).show();
+                    }
                 }
                 catch (Exception e){
                     Toast.makeText(dangNhap.this, "Đăng nhập Fail", Toast.LENGTH_SHORT).show();
